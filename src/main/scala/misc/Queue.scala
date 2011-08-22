f313084ef53b44e31080f9ede4bd0b7a771fd568
Patric Fornasier <patric.fornasier@gmail.com>
2011-08-22 16:34:46 +0100

    added queue

--- src/main/scala/misc/Queue.scala
+++ src/main/scala/misc/Queue.scala
@@ -4,6 +4,8 @@ object Queue {
   def apply[A](elements : A *) = new Queue[A](elements.toList)
 }
 
-class Queue[A] private (elements:List[A]) {
+class Queue[+A] private (elements:List[A]) {
   def dequeue: (A, Queue[A]) = elements.head -> Queue(elements.tail :_*)
+  
+  def enqueue[B >: A](element: B): Queue[B] = Queue(element :: elements :_*)
 }
